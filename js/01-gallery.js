4fd5ca41a9df8b21b6c6f50e08d173d83b6ec00f
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-02-08 19:26:10 +0200

    рендерит разметку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+    </a>
+    </div>`;
+    })
+    .join("");
+}
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+// gallery.insertAdjacentElement("beforeend", galleryItemMarkup);
+gallery.innerHTML = galleryItemMarkup;
+
+gallery.addEventListener("click", (event) => {
+    preventDefault()
+});
