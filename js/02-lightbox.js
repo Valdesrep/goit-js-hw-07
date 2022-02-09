275265d33529c78849f860000bf856239af59b75
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-02-09 21:40:09 +0200

    рендер разметки и подключение библиотеки таск 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"/>
+        </a>`;
+    })
+    .join("");
+}
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+gallery.innerHTML = galleryItemMarkup;
